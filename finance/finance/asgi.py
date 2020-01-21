5cef9ce377361cf930eae5f2dac2028aa87bd949
Mykola Matviiv <matviivcareer@gmail.com>
2020-01-21 18:56:01 +0200

    quick fix

--- finance/finance/asgi.py
+++ finance/finance/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for Finance_Helper project.
+ASGI config for finance project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Finance_Helper.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'finance.settings')
 
 application = get_asgi_application()
