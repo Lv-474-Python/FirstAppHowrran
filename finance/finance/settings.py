abb26c32381dcd95a41b204f303534032d5dfa5f
Mykola Matviiv <matviivcareer@gmail.com>
2020-01-27 23:40:37 +0200

    url fixes

--- finance/finance/settings.py
+++ finance/finance/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'account',
+    'category',
 ]
 AUTH_USER_MODEL = "account.CustomUser"
 
--- finance/finance/urls.py
+++ finance/finance/urls.py
@@ -18,4 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("account/", include('account.urls')),
+    path("category/", include('category.urls')),
 ]
