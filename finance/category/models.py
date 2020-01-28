15774e5a2b1b5325ed2a82f812793f62201ffc1c
Mykola Matviiv <matviivcareer@gmail.com>
2020-01-28 17:36:45 +0200

    added category creation and display

--- finance/category/models.py
+++ finance/category/models.py
@@ -1,7 +1,9 @@
 from django.db import models, IntegrityError
+from account.models import CustomUser
+
 
 class Category(models.Model):
-    user_id = models.IntegerField()
+    user_id = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     name = models.CharField(max_length=255)
     type = models.CharField(max_length=255)
     month_limit = models.FloatField(max_length=1000000)
@@ -11,8 +13,9 @@ class Category(models.Model):
         db_table = 'tbl_category'
 
     @staticmethod
-    def create(user_id, name, type, month_limit=999_999_999_999):
+    def create(user_id, name, type, description, month_limit=999_999_999_999):
         category = Category(user_id=user_id, name=name, type=type,
+                            description=description,
                             month_limit=month_limit)
 
         try:
--- finance/category/views.py
+++ finance/category/views.py
@@ -1,7 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Category
+
+
 def home_view(request):
-    return render(request, 'category.html')
+    categories = Category.objects.all()
+    user_categories = Category.objects.filter(user_id=request.user)
+    print(user_categories)
+
+    return render(request, 'category.html', {"categories": categories,
+                                             'user_categories': user_categories})
+
 
 def create_view(request):
-    pass
+    if request.method == 'POST':
+
+        data = {
+            "user_id": request.user,
+            "name": request.POST.get('name'),
+            "type": request.POST.get('category_type'),
+            "description": request.POST.get('category_description')
+        }
+
+        month_limit = request.POST.get('month_limit')
+        if month_limit:
+            data["month_limit"] = month_limit
+
+        Category.create(**data)
+        return redirect('home_category')
+
+    return render(request, 'create_category.html')
