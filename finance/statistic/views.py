09aa389cbbdea3c729942f48dfa046d659efcaea
Mykola Matviiv <matviivcareer@gmail.com>
2020-02-04 18:01:10 +0200

    added piechart plot

--- finance/statistic/views.py
+++ finance/statistic/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from operation.models import Operation
 from category.models import Category
+from plotly.offline import plot
+import plotly.express as px
 
 
 def home_view(request):
+    categories, income = Operation.get_user_income_by_category(request.user)
+
+    fig = px.pie(categories, categories, income)
+    # plot(fig, filename='statistic/templates/piechart_income')
+    chart = plot(fig, output_type='div')
+
     income = Operation.get_user_income(request.user)
     outcome = Operation.get_user_outcome(request.user)
     current = Operation.get_user_current(request.user)
@@ -11,13 +19,15 @@ def home_view(request):
     user_category = Category.get_user_category(request.user)
 
     return render(request, 'statistic.html',
-                  {'income': income, 'outcome': outcome, 'current': current,
+                  {'income': income,
+                   'outcome': outcome,
+                   'current': current,
                    'operation_list': operation_list,
-                   'user_category': user_category})
+                   'user_category': user_category,
+                   'chart':chart})
 
 
 def statistic_category_view(request, category_id):
-
     category = Category.get_category(category_id)
     category_name = category.name
     operation_list = Operation.get_user_operation_by_category(request.user,
@@ -29,7 +39,7 @@ def statistic_category_view(request, category_id):
                                                            category_id)
 
     return render(request, 'statistic_category.html',
-                  {'category_name':category_name,
+                  {'category': category,
                    'operation_list': operation_list,
-                   'category_income':category_income,
-                   'category_outcome':category_outcome})
+                   'category_income': category_income,
+                   'category_outcome': category_outcome})
